c9cc14f8fc706df6953b06be4869283a727f4556
장정운 <jjangnotjang@gmail.com>
2025-05-29 17:37:56 +0900

    design: 교내 홍보   퍼블리싱

--- app/page.tsx
+++ app/page.tsx
@@ -263,6 +263,24 @@ export default function Home() {
         </div>
       </div>
 
+      {/* 교내 홍보 */}
+      <div className='px-4 mb-8'>
+        <div className='flex justify-between items-center mb-4'>
+          <h2 className='text-xl font-bold'>교내 홍보</h2>
+          <button className='text-[#417EFF] text-sm font-semibold whitespace-nowrap'>
+            더 보기 &gt;
+          </button>
+        </div>
+
+        <div className='flex gap-3 overflow-x-auto hide-scrollbar'>
+          {[1, 2, 3].map((_, idx) => (
+            <div
+              key={idx}
+              className='w-[160px] h-[180px] rounded-xl bg-white flex-shrink-0'
+            />
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
