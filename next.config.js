d282e0a8cd94bff384de9372bd15006397f01e4f
장정운 <jjangnotjang@gmail.com>
2025-08-02 02:09:56 +0900

    경로 수정

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
 module.exports = {
   async rewrites() {
     return [
-      // 1) schools 검색을 /api/v1/schools로 보내기
       {
-        source: '/api/v1/schools/:path*',
-        destination: 'https://hiteen.site/api/v1/schools/:path*',
+        source: '/api/schools/:path*',
+        destination: 'https://hiteen.site/schools/:path*',
       },
-      // 2) 그 외 /api/... 은 그대로 /api/... 로 보내기
       {
         source: '/api/:path*',
         destination: 'https://hiteen.site/api/:path*',
       },
     ];
   },
-};
+}
